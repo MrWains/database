dd1576601d5a0a97f19e151e63d5777347eb0a59
Irtasam Ali Wains <irtasamali@gmail.com>
2021-11-17 21:26:37 +0500

    Deployed

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,44 @@
 import React from "react";
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import Axios from "axios";
-import './App.css';
+import "./App.css";
 
 function App() {
   // state variables
   const [id, setId] = useState("");
   const [name, setName] = useState("");
 
-  const logIn = () => 
-  {
-    Axios.post("address", {ID:id, Name:name})
-    .then(() => 
-    {
-      alert("Sucessful Insert!")
-    })
-  }
-
-
+  const logIn = () => {
+    Axios.post("https://healthatlums-database.herokuapp.com/api/insert", {
+      ID: id,
+      Name: name,
+    }).then(() => {
+      alert("Sucessful Insert!");
+    });
+  };
 
   return (
     <div className="App">
-      
       <h2>Log In</h2>
 
       <label>ID</label>
-      <input type="text" name="ID" onChange={(e)=>{setId(e.target.value)}}></input>
+      <input
+        type="text"
+        name="ID"
+        onChange={(e) => {
+          setId(e.target.value);
+        }}
+      ></input>
       <label>name</label>
-      <input type="text" name="name" onChange={(e)=>{setName(e.target.value)}}></input>
+      <input
+        type="text"
+        name="name"
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+      ></input>
 
       <button onClick={logIn()}></button>
-      
-
     </div>
   );
 }
