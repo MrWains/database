2e13af932976fcaba01694d8485214255e96fb61
Irtasam Ali Wains <irtasamali@gmail.com>
2021-11-19 13:11:38 +0500

    url change

--- client/src/Add_Doctor.js
+++ client/src/Add_Doctor.js
@@ -12,72 +12,96 @@ function Add_Doctor() {
   const [middleName, setmiddleName] = useState("");
   const [lastName, setlastName] = useState("");
   const [specialization, setSpecialization] = useState("");
- 
 
   const [roleidstate, setroleidstate] = useState("/adddoctor");
 
-
   const adddoctor = () => {
     // Axios.post("https://healthatlums-database.herokuapp.com/api/insert/", {ID: id, Name: name,})
-    Axios.post("http://localhost:3001/api/adddoctor", {workerID:workerID , role_id:role_id ,firstName: firstName, middleName: middleName, lastName:lastName, specialziation: specialization})
-    .then(() => 
-    {
-      console.log("record_added")
+    Axios.post("https://healthatlums-database.herokuapp.com/api/adddoctor", {
+      workerID: workerID,
+      role_id: role_id,
+      firstName: firstName,
+      middleName: middleName,
+      lastName: lastName,
+      specialziation: specialization,
     })
-    .catch((err) => 
-    {
-      console.log(err);
-    })
+      .then(() => {
+        console.log("record_added");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   return (
     <div className="App">
-      <div className = "homepage">
+      <div className="homepage">
         <h2>Add Doctor</h2>
 
         <label>worker ID</label>
-        <input type="text" name="ID" onChange={(e) => {
-          setworkerID(e.target.value);
-        }}/>
+        <input
+          type="text"
+          name="ID"
+          onChange={(e) => {
+            setworkerID(e.target.value);
+          }}
+        />
 
         <label>role_id</label>
-        <input type="text" name="ID" onChange={(e) => {
-          setrole_id(e.target.value);
-        }}/>
+        <input
+          type="text"
+          name="ID"
+          onChange={(e) => {
+            setrole_id(e.target.value);
+          }}
+        />
 
         <label>First Name</label>
-        <input type="text" name="ID" onChange={(e) => {
-          setfirstName(e.target.value);
-        }}/>
-  
+        <input
+          type="text"
+          name="ID"
+          onChange={(e) => {
+            setfirstName(e.target.value);
+          }}
+        />
+
         <label>Middle Name</label>
-        <input type="text" name="Password" onChange={(e) => 
-          {setmiddleName(e.target.value);
-        }}/>
+        <input
+          type="text"
+          name="Password"
+          onChange={(e) => {
+            setmiddleName(e.target.value);
+          }}
+        />
 
         <label>Last Name</label>
-        <input type="text" name="ID" onChange={(e) => {
-          setlastName(e.target.value);
-        }}/>
-
+        <input
+          type="text"
+          name="ID"
+          onChange={(e) => {
+            setlastName(e.target.value);
+          }}
+        />
 
         <label>Specialization</label>
-        <input type="text" name="ID" onChange={(e) => {
-          setSpecialization(e.target.value);
-        }}/>
+        <input
+          type="text"
+          name="ID"
+          onChange={(e) => {
+            setSpecialization(e.target.value);
+          }}
+        />
 
-        <NavLink className="nav-link" to = "/adddoctor">  
-        <button onClick={adddoctor} >Add doctor</button>
+        <NavLink className="nav-link" to="/adddoctor">
+          <button onClick={adddoctor}>Add doctor</button>
         </NavLink>
 
-        <NavLink className="nav-link" to = "/home">  
-        <button  >Back</button>
+        <NavLink className="nav-link" to="/home">
+          <button>Back</button>
         </NavLink>
-        
       </div>
     </div>
   );
-
 }
 
 export default Add_Doctor;
--- client/src/Add_Student.js
+++ client/src/Add_Student.js
@@ -18,88 +18,132 @@ function Add_Student() {
   const [role_id, setrole_id] = useState(0);
   const [roleidstate, setroleidstate] = useState("/addstudent");
 
-
   const addStudent = () => {
     // Axios.post("https://healthatlums-database.herokuapp.com/api/insert/", {ID: id, Name: name,})
-    Axios.post("http://localhost:3001/api/addstudent", {firstName: firstName, middleName: middleName, lastName:lastName, batch: batch, contactNumber: contactNumber, emergency: emergency, rollNumber: rollNumber, role_id:role_id, emergencyFName: emergencyFName, emergencyLName: emergencyLName})
-    .then(() => 
-    {
-      console.log("record_added")
+    Axios.post("https://healthatlums-database.herokuapp.com/api/addstudent", {
+      firstName: firstName,
+      middleName: middleName,
+      lastName: lastName,
+      batch: batch,
+      contactNumber: contactNumber,
+      emergency: emergency,
+      rollNumber: rollNumber,
+      role_id: role_id,
+      emergencyFName: emergencyFName,
+      emergencyLName: emergencyLName,
     })
-    .catch((err) => 
-    {
-      console.log(err);
-    })
+      .then(() => {
+        console.log("record_added");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   return (
     <div className="App">
-      <div className = "homepage">
+      <div className="homepage">
         <h2>Add Student</h2>
         <label>First Name</label>
-        <input type="text" name="ID" onChange={(e) => {
-          setfirstName(e.target.value);
-        }}/>
-  
+        <input
+          type="text"
+          name="ID"
+          onChange={(e) => {
+            setfirstName(e.target.value);
+          }}
+        />
+
         <label>Middle Name</label>
-        <input type="text" name="Password" onChange={(e) => 
-          {setmiddleName(e.target.value);
-        }}/>
+        <input
+          type="text"
+          name="Password"
+          onChange={(e) => {
+            setmiddleName(e.target.value);
+          }}
+        />
 
         <label>Last Name</label>
-        <input type="text" name="ID" onChange={(e) => {
-          setlastName(e.target.value);
-        }}/>
-
+        <input
+          type="text"
+          name="ID"
+          onChange={(e) => {
+            setlastName(e.target.value);
+          }}
+        />
 
         <label>Roll Number</label>
-        <input type="text" name="ID" onChange={(e) => {
-          setrollNumber(e.target.value);
-        }}/>
-  
+        <input
+          type="text"
+          name="ID"
+          onChange={(e) => {
+            setrollNumber(e.target.value);
+          }}
+        />
+
         <label>Contact Number</label>
-        <input type="text" name="Password" onChange={(e) => 
-          {setcontactNumber(e.target.value);
-        }}/>
+        <input
+          type="text"
+          name="Password"
+          onChange={(e) => {
+            setcontactNumber(e.target.value);
+          }}
+        />
 
         <label>Emergency Contact Number</label>
-        <input type="text" name="Password" onChange={(e) => 
-          {setEmergency(e.target.value);
-        }}/>
+        <input
+          type="text"
+          name="Password"
+          onChange={(e) => {
+            setEmergency(e.target.value);
+          }}
+        />
 
         <label>Batch</label>
-        <input type="text" name="ID" onChange={(e) => {
-          setBatch(e.target.value);
-        }}/>
-  
+        <input
+          type="text"
+          name="ID"
+          onChange={(e) => {
+            setBatch(e.target.value);
+          }}
+        />
+
         <label>Role_ID</label>
-        <input type="text" name="Password" onChange={(e) => 
-          {setrole_id(e.target.value);
-        }}/>
+        <input
+          type="text"
+          name="Password"
+          onChange={(e) => {
+            setrole_id(e.target.value);
+          }}
+        />
 
         <label>Emergency First Name</label>
-        <input type="text" name="Password" onChange={(e) => 
-          {setEmergencyFName(e.target.value);
-            }}/>
+        <input
+          type="text"
+          name="Password"
+          onChange={(e) => {
+            setEmergencyFName(e.target.value);
+          }}
+        />
 
         <label>Emergency Last Name</label>
-        <input type="text" name="Password" onChange={(e) => 
-          {setEmergencyLName(e.target.value);
-        }}/>
+        <input
+          type="text"
+          name="Password"
+          onChange={(e) => {
+            setEmergencyLName(e.target.value);
+          }}
+        />
 
-
-        <NavLink className="nav-link" to = "/addstudent">  
-        <button onClick={addStudent} >Add a student</button>
+        <NavLink className="nav-link" to="/addstudent">
+          <button onClick={addStudent}>Add a student</button>
         </NavLink>
 
-        <NavLink className="nav-link" to = "/home">  
-        <button  >Back</button>
+        <NavLink className="nav-link" to="/home">
+          <button>Back</button>
         </NavLink>
-        
       </div>
     </div>
   );
-
 }
 
 export default Add_Student;
